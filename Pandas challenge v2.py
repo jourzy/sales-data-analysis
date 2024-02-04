2053c7dda63dbb618c83c2a4c335eeecfcf382b5
Jourzy <emmanora@hotmail.com>
2024-02-04 21:53:43 +0000

    Created my own functions for the tasks

--- Pandas challenge v2.py
+++ Pandas challenge v2.py
@@ -2,9 +2,6 @@
 # April 2023
 # Emma Jourzac
 
-# Import the library to use in this program
-import pandas as pd
-
 
 # Imagine you work for a small company that sells products online.
 # Your manager has asked you to analyse the sales data from the past year to determine
@@ -23,21 +20,85 @@ import pandas as pd
 # 5. Determine which customers made the most purchases and how much they spent in total
 # 6. Write the results of your analysis as a CSV file
 
+# Optional extras:
+#
+# Use Matplotlib or Seaborn to create detailed graphs of the sales data
+# Calculate additional metrics (e.g. median sale price, standard deviation of sale prices)
+
+
+# ----- importing libraries
+import pandas as pd
+
+# ----- helper functions
+
+
+def read_csv(file):
+    # Read in the data from a csv file
+    result = pd.read_csv(file)
+    return result
+
+
+def frequency(data_frame, value):
+    # checking the frequency of the value in the data set
+    result = data_frame[value].value_counts()
+    return result
+
+
+def calc_product(data_frame, new_column, col1, col2):
+    # multiplies the columns together and creates a new column with the result
+    data_frame[new_column] = data_frame[col1] * data_frame[col2]
+
+
+def avg(data_frame, col1="Category", col2="Sale Price"):
+    # used to find the average in a category.
+    # Sales Price used but could also use for other fields.
+    # Groups the data by col1.
+    # Calculates the mean value for the col2 data.
+    # Rounded to 2 d.p.
+    result = data_frame.groupby([col1])[col2].mean(numeric_only=True).round(2)
+    return result
+
+
+def group_sum(data_frame, col1="Month", col2="Total Sales"):
+    # Group data by col1 e.g. "Month"
+    # Add up all of col2. e.g. "Total Sales"
+    result = data_frame.groupby([col1])[col2].sum()
+    # Adds the index back in - needed to return the index containing highest and lowest values
+    result = result.reset_index()
+    return result
+
+
+def highest(result, col2="Total Sales"):
+    # Locates the index with the highest value e.g. "total sales"
+    # and returns the data in this row - including month name.
+    maximum = result.loc[result[col2].idxmax()]
+    return maximum
+
+
+def lowest(result, col2="Total Sales"):
+    # Locates the index with the lowest value e.g. "total sales"
+    # and returns the data in this row - including month name.
+    minimum = result.loc[result[col2].idxmin()]
+    return minimum
+
+
+# ----- main program
+
 # ----- 1. USE PANDAS TO READ IN THE SALES DATA FROM THE CSV FILE
 
-# Read in the sales data from the csv file
-df = pd.read_csv("sales_dataset.csv")
+# Read in the sales data from the csv file and print out results
+df = read_csv("sales_dataset.csv")
 print(df)
-
-# checking that there is only one version of each product in the data
-df["Product Name"].value_counts()
-
 
 # ----- 2. CALCULATE THE TOTAL SALES FOR EACH PRODUCT
 
-# You can create a new column of data like this:
-df["Total Sales"] = df["Sale Price"] * df["Quantity Sold"]
+# checking that there is only one version of each product in the data
+frequency = frequency(df, "Product Name")
+print(frequency)
 
+# creates a new column called "Total Sales" by multiplying
+# together "Quantity sold" and "Sale Price"
+calc_product(df, "Total Sales", "Sale Price", "Quantity Sold")
 print(df)
 
 # ----- 6. WRITE THE RESULTS OF YOUR ANALYSIS AS A CSV FILE
@@ -47,38 +108,31 @@ df.to_csv("2.total_sales.csv", index=True)
 
 # ----- 3. DETERMINE THE AVERAGE SALE PRICE FOR EACH PRODUCT CATEGORY
 
-# Groups the data by Category. 
-# Calculates the mean value for the Sale Price data. 
-# Rounded to 2 d.p.
-avg_price = df.groupby(["Category"])["Sale Price"].mean(numeric_only=True).round(2)
+# finding the average sale price for each category
+avg_price = avg(df, "Category", "Sale Price")
 print(avg_price)
 
-# ----- 6. WRITE THE RESULTS OF YOUR ANALYSIS AS A CSV FILE
 
 # saving the DataFrame as a CSV file
 avg_price.to_csv("3.avg_price.csv", index=True)
 
-# ----- 4. IDENTIFY THE MONTH WITH THE HIGHEST SALES AND THE MONTH WITH THE LOWEST SALES
+# ----- 4. IDENTIFY THE MONTH WITH THE HIGHEST SALES
+#          AND THE MONTH WITH THE LOWEST SALES
 
-# Group data by month.
-# Add up all sales for that month
-month_total = df.groupby(["Month"])["Total Sales"].sum()
-print(month_total)
+# calculates the total sales grouped by month
+month_total_sales = group_sum(df, "Month", "Total Sales")
+print(month_total_sales)
 
-# Adds the index back in - needed to return the index containing highest and lowest values
-month_total = month_total.reset_index()
-
-print(month_total)
-
-# Locates the index with the highest total sales and returns the data in this row - including month name.
-max_month = month_total.loc[month_total["Total Sales"].idxmax()]
+# Locates the index with the highest value e.g. "total sales"
+# and returns the data in this row - including month name.
+max_month = highest(month_total_sales, "Total Sales")
 print(max_month)
 
 # saving the analysis as a CSV file
 max_month.to_csv("4a.max_month.csv", index=True)
 
 # Does the same for the minimum value
-min_month = month_total.loc[month_total["Total Sales"].idxmin()]
+min_month = lowest(month_total_sales, "Total Sales")
 print(min_month)
 
 # saving the analysis as a CSV file
@@ -87,12 +141,12 @@ min_month.to_csv("4b.min_month.csv", index=True)
 
 # ----- 5. DETERMINE WHICH CUSTOMER MADE THE MOST PURCHASES AND HOW MUCH THEY SPENT IN TOTAL
 
-# Adds up all purchases (quantity sold) grouped by customer name.
-customer = df.groupby(["Customer Name"])[["Quantity Sold", "Total Sales"]].sum(numeric_only=True)
-print(customer)
+# finds total number of purchases and total sales by customer.
+customer_purchases = group_sum(df, "Customer Name", ["Quantity Sold", "Total Sales"])
+print(customer_purchases)
 
 # Finds the customer with the highest number of purchases.
-customer_max = customer.loc[customer["Quantity Sold"].idxmax()]
+customer_max = highest(customer_purchases, "Quantity Sold")
 print(customer_max)
 
 # saving the DataFrame as a CSV file
